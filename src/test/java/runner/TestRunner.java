86f55397e3fcbd98777f61ef87999e4e3c6649c3
BCP <bcp@PE-IT001279.bbrouter>
2024-03-06 15:03:08 -0500

    github_9

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,14 +1,17 @@
 package runner;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", //Directorio de nuestros archivos .feature
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(publish = true,
+        features = "src/test/resources/features", //Directorio de nuestros archivos .feature
         glue ="stepdefinitions", // Paquete donde tenemos nuestras clases
-        plugin={"pretty","html:target/cucumber-reports"},
-        tags = "@Un_Happy_Paths_03"
+        tags = "@Un_Happy_Paths_03",
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+
 )
 public class TestRunner {
 }
+
